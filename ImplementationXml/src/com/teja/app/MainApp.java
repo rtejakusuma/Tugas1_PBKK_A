e1ebe9779d988918c3c33871337490486740e0f1
RavenRindou <rtejakusuma21@yahoo.com>
2019-03-17 23:38:11 +0700

    update dikit

--- ImplementationXml/src/com/teja/app/MainApp.java
+++ ImplementationXml/src/com/teja/app/MainApp.java
@@ -1,7 +1,5 @@
 package com.teja.app;
 
-import org.springframework.context.ApplicationContext;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.teja.model.*;
@@ -10,12 +8,14 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext context = new ClassPathXmlApplicationContext("HeroesConfig.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("HeroesConfig.xml");
 		Heroes hero1 = context.getBean("h1", Heroes.class);
 		System.out.println(hero1);
 		
 		Heroes hero2 = context.getBean("h2", Heroes.class);
 		System.out.println(hero2);
+		
+		context.close();
 	}
 
 }
